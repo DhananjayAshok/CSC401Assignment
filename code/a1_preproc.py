42f24c9cee56ff814f3615bdbfe41705d193f031
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2022-02-11 01:52:55 -0500

    Finished Preproc

--- code/a1_preproc.py
+++ code/a1_preproc.py
@@ -15,10 +15,24 @@ import os
 import json
 import re
 import spacy
+import html
 
 
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 nlp.add_pipe('sentencizer')
+
+
+def token_utility(token):
+    if token.lemma_ is None or len(token.lemma_) == 0:
+        first = token.text
+    elif token.lemma_[0] == "-" and (len(token.text) > 0 and token.text[0] !="-"):
+        first = token.text
+    else:
+        if token.text == token.text.upper():
+            first = token.lemma_.upper()
+        else:
+            first = token.lemma_.lower()
+    return first + "/" + token.tag_
 
 
 def preproc1(comment , steps=range(1, 6)):
@@ -35,45 +49,58 @@ def preproc1(comment , steps=range(1, 6)):
     if 1 in steps:  
         #modify this to handle other whitespace chars.
         #replace newlines with spaces
-        modComm = re.sub(r"\n{1,}", " ", modComm)
+        nonwhitespaces = ["\n", "\t", "\r", "\f", "\v"]
+        for item in nonwhitespaces:
+            modComm = re.sub(item, " ", modComm)
 
     if 2 in steps:  # unescape html
-        print("TODO")
+        modComm = html.unescape(modComm)
 
     if 3 in steps:  # remove URLs
         modComm = re.sub(r"\b(http:\/\/|https:\/\/|www\.)\S+", "", modComm)
         
     if 4 in steps: #remove duplicate spaces.
-        print("TODO") 
+        modComm = re.sub(' +', ' ', modComm)
 
     if 5 in steps:
-        print("TODO")
-        # TODO: get Spacy document for modComm
-        
-        # TODO: use Spacy document for modComm to create a string.
-        # Make sure to:
-        #    * Insert "\n" between sentences.
-        #    * Split tokens with spaces.
-        #    * Write "/POS" after each token.
+        # Make sure to: Split tokens with spaces.
+        utt = nlp(modComm)
+        totalComm = ""
+        for i, sent in enumerate(utt.sents):
+            if i != 0:
+                totalComm += "\n "
+            for j, token in enumerate(sent):
+                print(f"\tLooking at sentence {i} token {j}: {token.text} has lemma {token.lemma_} and tag {token.tag_}")
+                if token.text in [" ", "\t", "\n", "\r", "\f", "\v"]:  # If the token is whitespace ignore but don't add
+                    continue
+                if j != 0:
+                    totalComm += " "
+                totalComm += token_utility(token)
+        modComm = totalComm
     return modComm
 
 
 def main(args):
     allOutput = []
+    print("Walking through ", indir, " ", list(os.walk(indir)))
     for subdir, dirs, files in os.walk(indir):
         for file in files:
             fullFile = os.path.join(subdir, file)
             print( "Processing " + fullFile)
 
-            data = json.load(open(fullFile))
-            # TODO: select appropriate args.max lines
-            # TODO: read those lines with something like `j = json.loads(line)`
-            # TODO: choose to retain fields from those lines that are relevant to you
-            # TODO: add a field to each selected line called 'cat' with the value of 'file' (e.g., 'Alt', 'Right', ...) 
-            # TODO: process the body field (j['body']) with preproc1(...) using default for `steps` argument
-            # TODO: replace the 'body' field with the processed text
-            # TODO: append the result to 'allOutput'
-            
+            data = json.load(open(fullFile))[:args.max]
+            possible_keys = ['author_flair_text', 'parent_id', 'author_flair_css_class', 'link_id', 'ups', 'id', 'score_hidden', 'author', 'gilded', 'controversiality', 'name', 'downs', 'archived', 'created_utc', 'score', 'distinguished', 'subreddit_id', 'retrieved_on', 'body', 'subreddit', 'edited']
+            useless_keys = possible_keys#['parent_id', 'author_flair_css_class', 'link_id', 'id',  'subreddit_id']
+            for i, line in enumerate(data):
+                j = json.loads(line)
+                keys = list(j.keys())
+                for key in useless_keys:
+                    if key in keys and key != "body":
+                        j.pop(key)
+                j["cat"] = file
+                j["body"] = preproc1(j["body"])
+                allOutput.append(j)
+    print(f"allOutput length: {len(allOutput)}")
     fout = open(args.output, 'w')
     fout.write(json.dumps(allOutput))
     fout.close()
@@ -94,4 +121,5 @@ if __name__ == "__main__":
         sys.exit(1)
     
     indir = os.path.join(args.a1_dir, 'data')
+    print(args)
     main(args)
